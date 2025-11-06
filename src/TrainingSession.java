2a9b874f4ec214662ae7135348ac6488cfcbf681
Damir Vafin <damirvafinnailevich@gmail.com>
2025-11-06 14:59:20 +0300

    Переопределить equals(), hashCode(), toString() в классе TrainingSession

--- src/TrainingSession.java
+++ src/TrainingSession.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class TrainingSession {
 
     //группа
@@ -30,5 +32,31 @@ public class TrainingSession {
 
     public TimeOfDay getTimeOfDay() {
         return timeOfDay;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrainingSession that = (TrainingSession) o;
+        return Objects.equals(group, that.group) &&
+                Objects.equals(coach, that.coach) &&
+                dayOfWeek == that.dayOfWeek &&
+                Objects.equals(timeOfDay, that.timeOfDay);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(group, coach, dayOfWeek, timeOfDay);
+    }
+
+    @Override
+    public String toString() {
+        return "TrainingSession{" +
+                "group=" + group.getTitle() +
+                ", coach=" + coach +
+                ", dayOfWeek=" + dayOfWeek +
+                ", timeOfDay=" + timeOfDay +
+                '}';
     }
 }
