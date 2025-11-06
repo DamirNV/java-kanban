ee91aae31c60771bdca706a03b8592fe3f59d474
Damir Vafin <damirvafinnailevich@gmail.com>
2025-11-06 18:09:00 +0300

    Заменить русскую букву на английскую названии метода сountByCoaches()

--- src/ui/GymManagementApp.java
+++ src/ui/GymManagementApp.java
@@ -29,7 +29,7 @@ public class GymManagementApp {
                     showTrainingSessionsForDayAndTime();
                     break;
                 case 4:
-                    сountByCoaches();
+                    countByCoaches();
                     break;
                 case 5:
                     running = false;
@@ -212,7 +212,7 @@ public class GymManagementApp {
 
     }
 
-    private static void сountByCoaches() {
+    private static void countByCoaches() {
         Map<Coach, Integer> coachStats = timetable.getCountByCoaches();
         System.out.println("\n=== Статистика тренеров за неделю ===");
         if (coachStats.isEmpty()) {
