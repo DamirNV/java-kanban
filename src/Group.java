c2b6f137d676355085c3087b724d982e0ebc13e7
Damir Vafin <damirvafinnailevich@gmail.com>
2025-11-06 16:35:57 +0300

    Переопределить equals(), hashCode() в классе Group

--- src/Group.java
+++ src/Group.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Group {
     //название группы
     private String title;
@@ -23,4 +25,20 @@ public class Group {
     public int getDuration() {
         return duration;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return duration == group.duration &&
+                Objects.equals(title, group.title) &&
+                age == group.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, age, duration);
+    }
+
 }
